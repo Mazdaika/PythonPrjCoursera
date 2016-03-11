6542da945a221d79362d8448e53cec3103cd2623
Igor Arkhipov <ig.arkhipov@gmail.com>
2016-03-11 19:55:21 +1100

    - assignment 9-4 - functions: default file name added

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-def fineopen():
+def fineopen(defaultfile = "mbox-short.txt"):
     fnvalid = False
     fh = None
     while not fnvalid:
         try:
             fname = raw_input("Enter file name: ")
+            if fname == "" : return open(defaultfile)
             fh = open(fname)
             fnvalid = True
         except:
