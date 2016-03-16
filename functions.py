56b8400e012d62ff3b322edc1e8a46727f79d110
Igor Arkhipov <ig.arkhipov@gmail.com>
2016-03-16 21:07:27 +1100

    - assignment course 3 week 4: following links

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import urllib
+
 def fineopen(defaultfile = "mbox-short.txt"):
     fnvalid = False
     fh = None
@@ -10,3 +12,17 @@ def fineopen(defaultfile = "mbox-short.txt"):
         except:
             print "Enter a valid file name!"
     return fh
+
+def fineurlopen(defaultvalue="http://python-data.dr-chuck.net/known_by_Fikret.html"):
+    urlh = None
+    while True:
+        url = raw_input("Enter url:")
+        if url == "" :
+            urlh = urllib.urlopen(defaultvalue)
+            break
+        try:
+            urlh = urllib.urlopen(url)
+            break
+        except Exception as err:
+            print "Enter a valid url!\n" + err.message
+    return urlh
